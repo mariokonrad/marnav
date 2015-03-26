40bcc20245094b62b615c6c4dbc027ecbb4dea45
Mario Konrad <mario.konrad@gmx.net>
2015-03-26 22:27:48 +0100

    Unittest: more tests, more coverage.

--- src_test/nmea/Test_nmea_time.cpp
+++ src_test/nmea/Test_nmea_time.cpp
@@ -7,5 +7,47 @@ class Test_nmea_time : public ::testing::Test
 {
 };
 
-// TODO: implementation
+TEST_F(Test_nmea_time, explicit_construction)
+{
+	EXPECT_ANY_THROW((nmea::time{24, 0, 0, 0}));
+	EXPECT_ANY_THROW((nmea::time{0, 60, 0, 0}));
+	EXPECT_ANY_THROW((nmea::time{0, 0, 60, 0}));
+	EXPECT_ANY_THROW((nmea::time{0, 0, 0, 1000}));
+}
+
+TEST_F(Test_nmea_time, hour)
+{
+	nmea::time t{1, 2, 3, 4};
+
+	EXPECT_EQ(1u, t.hour());
+}
+
+TEST_F(Test_nmea_time, minutes)
+{
+	nmea::time t{1, 2, 3, 4};
+
+	EXPECT_EQ(2u, t.minutes());
+}
+
+TEST_F(Test_nmea_time, seconds)
+{
+	nmea::time t{1, 2, 3, 4};
+
+	EXPECT_EQ(3u, t.seconds());
+}
+
+TEST_F(Test_nmea_time, milliseconds)
+{
+	nmea::time t{1, 2, 3, 4};
+
+	EXPECT_EQ(4u, t.milliseconds());
+}
+
+TEST_F(Test_nmea_time, comparison_equal)
+{
+	nmea::time t0{1, 2, 3, 4};
+	nmea::time t1{1, 2, 3, 4};
+
+	EXPECT_TRUE(t0 == t1);
+}
 }
