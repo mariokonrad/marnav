7a545819577577bb5c92bb2f1c10d7043368d719
Mario Konrad <mario.konrad@gmx.net>
2015-07-29 11:08:54 +0200

    NMEA: definition of some enumerators for DSC.

--- src/marnav/nmea/dsc.hpp
+++ src/marnav/nmea/dsc.hpp
@@ -19,9 +19,9 @@ namespace nmea
 /// @endcode
 ///
 /// Field Number:
-/// 1.  Format Specifier
+/// 1.  Format Specifier (see ITU-R M493-13, table 3)
 /// 2.  Address / MMSI
-/// 3.  ?
+/// 3.  Category (see ITU-R M493-13, table 3)
 /// 4.  ?
 /// 5.  ?
 /// 6.  ?
@@ -38,6 +38,55 @@ class dsc : public sentence
 public:
 	constexpr static const sentence_id ID = sentence_id::DSC;
 	constexpr static const char * TAG = "DSC";
+
+	/// Format Specifier (see ITU-R M493-13, table 3)
+	enum class format_specifier : uint32_t {
+		geographical_are = 102, ///< Geographical, specifies an area
+		distress = 112, ///< Distress
+		all_ships = 116, ///< All Ships
+		individual_station = 120, ///< Individual Station
+	};
+
+	/// Category (see ITU-R M493-13, table 3)
+	enum class category : uint32_t {
+		routine = 100, ///< Routine Call
+		safety = 108, ///< Safety Call
+		urgency = 110, ///< Urgency Call
+		distress = 112 ///< Distress
+	};
+
+	/// Nature of distress (see ITU-R M493-13, table 3)
+	enum class distress_designation : uint32_t {
+		fire = 100, ///< Fire/explosion
+		flooding = 101, ///< Flooding
+		collision = 102, ///< Collision
+		grounding = 103, ///< Grounding
+		listing = 104, ///< Listing, danger of capsizing
+		sinking = 105, ///< Sinking
+		adrift = 106, ///< Disabled and adrift
+		undesignated = 107, ///< Undesignated Distress
+		abandoning = 108, ///< Abandoning ship
+		piracy = 109, ///< Piracy / armed robbery attack
+		mob = 110, ///< Man over board
+		epirb = 112, ///< EPIRB emission
+	};
+
+	/// First Telecommand (see ITU-R M493-13, table 3)
+	enum class first_telecommand : uint32_t {
+		polling = 103, ///< Polling
+		unable_to_comply = 104, ///< Unable to comply
+		end_of_call = 105, ///< End of call
+		data = 106, ///< Data
+		distress_ack = 110, ///< Distress acknowledgement
+		distress_relay = 112, ///< Distress relay
+		test = 118, ///< Test
+		no_information = 126, ///< No Information
+	};
+
+	/// Second Telecommand (see ITU-R M493-13, table 3)
+	enum class second_telecommand : uint32_t {
+		no_information = 126, ///< No Information
+	};
 
 	dsc();
 	dsc(const dsc &) = default;
