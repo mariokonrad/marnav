e2241aa6a02bc2a9e4e5b05385c20e5e403db47c
Mario Konrad <mario.konrad@gmx.net>
2015-08-03 13:26:36 +0200

    Math: minor enhancement in documentation of math/vector

--- src/marnav/math/vector.hpp
+++ src/marnav/math/vector.hpp
@@ -9,6 +9,10 @@ namespace marnav
 namespace math
 {
 
+/// A 2D vector.
+///
+/// @tparam T Basic data type, must be a floating point type.
+///
 template <typename T,
 	typename = typename std::enable_if<std::is_floating_point<T>::value, T>::type>
 class vector2
@@ -28,6 +32,10 @@ public:
 	vector2(vector2 &&) = default;
 
 	/// Returns a vector, initialized by the polar coordinates.
+	///
+	/// @param[in] radius Radius for the vector.
+	/// @param[in] phi Angle Phi in degrees.
+	/// @return A 2D vector.
 	static inline vector2 make_from_polar(value_type radius, value_type phi_deg)
 	{
 		const value_type phi = phi_deg * M_PI / 180.0;
@@ -46,17 +54,32 @@ public:
 	/// Returns the angle of polar coordinate in the interval 0..2pi in degrees
 	inline value_type phi_deg() const { return phi() * 180.0 / M_PI; }
 
+	/// Calculates the dot product.
 	inline value_type dot(const vector2 & v) const { return a[0] * v.a[0] + a[1] * v.a[1]; }
 
+	/// Returns the length of the vector.
 	inline value_type length() const { return sqrt(length2()); }
 
+	/// Returns the square of the length of the vector. This is useful if the
+	/// extra calculation of square root, done by the function 'length' is
+	/// not necessary or not desired.
 	inline value_type length2() const { return a[0] * a[0] + a[1] * a[1]; }
 
+	/// Normalizes the vector to a specific length.
 	inline vector2 & normalize(value_type len = 1.0)
 	{
 		const value_type l = length();
 		if (abs(l) > std::numeric_limits<value_type>::epsilon())
 			*this *= len / l;
+		return *this;
+	}
+
+	/// Set all components of the vector that are below epsilon to exact zero.
+	inline vector2 & nullify(void)
+	{
+		constexpr const value_type EPSILON = std::numeric_limits<value_type>::epsilon();
+		a[0] = abs(a[0]) <= EPSILON ? 0.0 : a[0];
+		a[1] = abs(a[1]) <= EPSILON ? 0.0 : a[1];
 		return *this;
 	}
 
@@ -75,10 +98,7 @@ public:
 		return (this == &v) || ((a[0] == v.a[0]) && (a[1] == v.a[1]));
 	}
 
-	inline bool operator!=(const vector2 & v) const
-	{
-		return !(*this == v);
-	}
+	inline bool operator!=(const vector2 & v) const { return !(*this == v); }
 
 	inline vector2 & operator+=(const vector2 & v)
 	{
@@ -114,28 +134,6 @@ public:
 private:
 	value_type a[2];
 };
-
-template <typename T> vector2<T> normalize_vec(const vector2<T> & v)
-{
-	vector2<T> t{v};
-	t.normalize();
-	return t;
-}
-
-template <typename T> T angle_between(const vector2<T> & v0, const vector2<T> & v1)
-{
-	return acos(v0.dot(v1) / (v0.length() * v1.length()));
-}
-
-template <typename T> vector2<T> project_onto(const vector2<T> & v0, const vector2<T> & v1)
-{
-	typename vector2<T>::value_type len = v1.length2();
-	if (abs(len) <= std::numeric_limits<typename vector2<T>::value_type>::epsilon())
-		return vector2<T>{};
-	return ((v0 * v1) / len) * v1;
-}
-
-using vec2 = vector2<double>;
 
 template <typename T,
 	typename = typename std::enable_if<std::is_floating_point<T>::value, T>::type>
@@ -177,12 +175,13 @@ public:
 			a[0] * v.a[1] - a[1] * v.a[0]);
 	}
 
+	/// Set all components of the vector that are below epsilon to exact zero.
 	inline vector3 & nullify(void)
 	{
-		const value_type EPSILON = std::numeric_limits<value_type>::epsilon();
-		a[0] = ((a[0] <= EPSILON) && (a[0] >= -EPSILON)) ? 0.0 : a[0];
-		a[1] = ((a[1] <= EPSILON) && (a[1] >= -EPSILON)) ? 0.0 : a[1];
-		a[2] = ((a[2] <= EPSILON) && (a[2] >= -EPSILON)) ? 0.0 : a[2];
+		constexpr const value_type EPSILON = std::numeric_limits<value_type>::epsilon();
+		a[0] = abs(a[0]) <= EPSILON ? 0.0 : a[0];
+		a[1] = abs(a[1]) <= EPSILON ? 0.0 : a[1];
+		a[2] = abs(a[2]) <= EPSILON ? 0.0 : a[2];
 		return *this;
 	}
 
@@ -232,8 +231,10 @@ public:
 
 	inline bool operator==(const vector3 & v) const
 	{
-		return ((a[0] == v.a[0]) && (a[1] == v.a[1]) && (a[2] == v.a[2]));
+		return (this == &v) || ((a[0] == v.a[0]) && (a[1] == v.a[1]) && (a[2] == v.a[2]));
 	}
+
+	inline bool operator!=(const vector3 & v) const { return !(*this == v); }
 
 	inline vector3 & operator+=(const vector3 & v)
 	{
@@ -259,41 +260,19 @@ public:
 		return *this;
 	}
 
-	friend vector3 operator+(const vector3 & w, const vector3 & v) { return vector3(w) += v; }
+	friend vector3 operator+(const vector3 & w, const vector3 & v) { return vector3{w} += v; }
 
-	friend vector3 operator-(const vector3 & w, const vector3 & v) { return vector3(w) -= v; }
+	friend vector3 operator-(const vector3 & w, const vector3 & v) { return vector3{w} -= v; }
 
-	friend vector3 operator*(const vector3 & v, value_type f) { return vector3(v) *= f; }
+	friend vector3 operator*(const vector3 & v, value_type f) { return vector3{v} *= f; }
 
-	friend vector3 operator*(value_type f, const vector3 & v) { return vector3(v) *= f; }
+	friend vector3 operator*(value_type f, const vector3 & v) { return vector3{v} *= f; }
 
 	friend value_type operator*(const vector3 & a, const vector3 & b) { return a.dot(b); }
 
 private:
 	value_type a[3];
 };
-
-template <typename T> vector3<T> normalize_vec(const vector3<T> & v)
-{
-	vector3<T> t{v};
-	t.normalize();
-	return t;
-}
-
-template <typename T> T angle_between(const vector3<T> & p0, const vector3<T> & p1)
-{
-	return acos(p0.dot(p1) / (p0.length() * p1.length()));
-}
-
-template <typename T> vector2<T> project_onto(const vector3<T> & v0, const vector3<T> & v1)
-{
-	typename vector3<T>::value_type len = v1.length2();
-	if (abs(len) <= std::numeric_limits<typename vector3<T>::value_type>::epsilon())
-		return vector3<T>{};
-	return ((v0 * v1) / len) * v1;
-}
-
-using vec3 = vector3<double>;
 
 template <typename T,
 	typename = typename std::enable_if<std::is_floating_point<T>::value, T>::type>
@@ -335,6 +314,17 @@ public:
 		return *this;
 	}
 
+	/// Set all components of the vector that are below epsilon to exact zero.
+	inline vector4 & nullify(void)
+	{
+		constexpr const value_type EPSILON = std::numeric_limits<value_type>::epsilon();
+		a[0] = abs(a[0]) <= EPSILON ? 0.0 : a[0];
+		a[1] = abs(a[1]) <= EPSILON ? 0.0 : a[1];
+		a[2] = abs(a[2]) <= EPSILON ? 0.0 : a[2];
+		a[3] = abs(a[3]) <= EPSILON ? 0.0 : a[3];
+		return *this;
+	}
+
 	inline value_type operator[](int idx) const { return a[idx]; }
 
 	inline vector4 & operator=(const vector4 &) = default;
@@ -343,8 +333,11 @@ public:
 
 	inline bool operator==(const vector4 & v) const
 	{
-		return ((a[0] == v.a[0]) && (a[1] == v.a[1]) && (a[2] == v.a[2]) && (a[3] == v.a[3]));
+		return (this == &v)
+			|| ((a[0] == v.a[0]) && (a[1] == v.a[1]) && (a[2] == v.a[2]) && (a[3] == v.a[3]));
 	}
+
+	inline bool operator!=(const vector4 & v) const { return !(*this == v); }
 
 	inline vector4 & operator+=(const vector4 & v)
 	{
@@ -386,15 +379,6 @@ public:
 private:
 	value_type a[4];
 };
-
-template <typename T> vector3<T> normalize_vec(const vector4<T> & v)
-{
-	vector4<T> t{v};
-	t.normalize();
-	return t;
-}
-
-using vec4 = vector4<double>;
 
 template <unsigned int N, typename T = double,
 	typename = typename std::enable_if<std::is_floating_point<T>::value, T>::type>
@@ -459,11 +443,15 @@ public:
 
 	inline bool operator==(const vector_n & v) const
 	{
+		if (this == &v)
+			return true;
 		for (unsigned int i = 0; i < N; ++i)
 			if (a[i] != v.a[i])
 				return false;
 		return true;
 	}
+
+	inline bool operator!=(const vector_n & v) const { return !(*this == v); }
 
 	inline vector_n & operator+=(const vector_n & v)
 	{
@@ -488,23 +476,90 @@ public:
 
 	friend vector_n operator+(const vector_n & w, const vector_n & v)
 	{
-		return vector_n(w) += v;
+		return vector_n{w} += v;
 	}
 
 	friend vector_n operator-(const vector_n & w, const vector_n & v)
 	{
-		return vector_n(w) -= v;
+		return vector_n{w} -= v;
 	}
 
-	friend vector_n operator*(const vector_n & v, value_type f) { return vector_n(v) *= f; }
+	friend vector_n operator*(const vector_n & v, value_type f) { return vector_n{v} *= f; }
 
-	friend vector_n operator*(value_type f, const vector_n & v) { return vector_n(v) *= f; }
+	friend vector_n operator*(value_type f, const vector_n & v) { return vector_n{v} *= f; }
 
 	friend value_type operator*(const vector_n & a, const vector_n & b) { return a.dot(b); }
 
 private:
 	value_type a[N];
 };
+
+using vec2 = vector2<double>;
+using vec3 = vector3<double>;
+using vec4 = vector4<double>;
+
+/// @{
+/// Returns a new constructed, normalized vector of the specified one.
+
+template <typename T> vector2<T> normalize_vec(const vector2<T> & v)
+{
+	vector2<T> t{v};
+	t.normalize();
+	return t;
+}
+
+template <typename T> vector3<T> normalize_vec(const vector3<T> & v)
+{
+	vector3<T> t{v};
+	t.normalize();
+	return t;
+}
+
+template <typename T> vector3<T> normalize_vec(const vector4<T> & v)
+{
+	vector4<T> t{v};
+	t.normalize();
+	return t;
+}
+
+/// @}
+
+/// @{
+/// Returns the angle between the two specified vectors in degrees.
+
+template <typename T> T angle_between(const vector2<T> & v0, const vector2<T> & v1)
+{
+	return acos(v0.dot(v1) / (v0.length() * v1.length()));
+}
+
+template <typename T> T angle_between(const vector3<T> & p0, const vector3<T> & p1)
+{
+	return acos(p0.dot(p1) / (p0.length() * p1.length()));
+}
+
+/// @}
+
+/// @{
+/// Returns a newly constructed vector which is a projection of \c v0 onto \c v1.
+
+template <typename T> vector2<T> project_onto(const vector2<T> & v0, const vector2<T> & v1)
+{
+	typename vector2<T>::value_type len = v1.length2();
+	if (abs(len) <= std::numeric_limits<typename vector2<T>::value_type>::epsilon())
+		return vector2<T>{};
+	return ((v0 * v1) / len) * v1;
+}
+
+template <typename T> vector2<T> project_onto(const vector3<T> & v0, const vector3<T> & v1)
+{
+	typename vector3<T>::value_type len = v1.length2();
+	if (abs(len) <= std::numeric_limits<typename vector3<T>::value_type>::epsilon())
+		return vector3<T>{};
+	return ((v0 * v1) / len) * v1;
+}
+
+/// @}
+
 }
 }
 
