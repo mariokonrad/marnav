0bb344c505fca55e61078fd4d19db9697526e6d9
Mario Konrad <mario.konrad@gmx.net>
2015-08-22 12:46:10 +0200

    NMEA: better test coverage

--- test/nmea/Test_nmea_dtm.cpp
+++ test/nmea/Test_nmea_dtm.cpp
@@ -83,4 +83,46 @@ TEST_F(Test_nmea_dtm, set_lon_offset_invalid_direction)
 	EXPECT_NO_THROW(dtm.set_lon_offset(1.0, nmea::direction::WEST));
 }
 
+TEST_F(Test_nmea_dtm, set_ref)
+{
+	nmea::dtm dtm;
+	dtm.set_ref("ABC");
+	EXPECT_STREQ("$GPDTM,ABC,,0.0000,N,0.0000,E,0.0,W84*74", nmea::to_string(dtm).c_str());
+}
+
+TEST_F(Test_nmea_dtm, set_ref_too_large)
+{
+	nmea::dtm dtm;
+	dtm.set_ref("ABCDEFGHI");
+	EXPECT_STREQ("$GPDTM,ABCDE,,0.0000,N,0.0000,E,0.0,W84*75", nmea::to_string(dtm).c_str());
+}
+
+TEST_F(Test_nmea_dtm, set_subcode)
+{
+	nmea::dtm dtm;
+	dtm.set_subcode("ABC");
+	EXPECT_STREQ("$GPDTM,W84,ABC,0.0000,N,0.0000,E,0.0,W84*2F", nmea::to_string(dtm).c_str());
+}
+
+TEST_F(Test_nmea_dtm, set_subcode_too_large)
+{
+	nmea::dtm dtm;
+	dtm.set_subcode("ABCDEFGHI");
+	EXPECT_STREQ("$GPDTM,W84,ABCDE,0.0000,N,0.0000,E,0.0,W84*2E", nmea::to_string(dtm).c_str());
+}
+
+TEST_F(Test_nmea_dtm, set_name)
+{
+	nmea::dtm dtm;
+	dtm.set_name("ABC");
+	EXPECT_STREQ("$GPDTM,W84,,0.0000,N,0.0000,E,0.0,ABC*74", nmea::to_string(dtm).c_str());
+}
+
+TEST_F(Test_nmea_dtm, set_name_too_large)
+{
+	nmea::dtm dtm;
+	dtm.set_name("ABCDEFGHI");
+	EXPECT_STREQ("$GPDTM,W84,,0.0000,N,0.0000,E,0.0,ABCDE*75", nmea::to_string(dtm).c_str());
+}
+
 }
