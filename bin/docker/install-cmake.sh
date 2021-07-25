c342be30bb6086b238cb1f0dad6322950cb9e3fe
Mario Konrad <mario.konrad@gmx.net>
2021-07-26 01:15:45 +0200

    Build: update docker images CMake/Boost

--- bin/docker/install-cmake.sh
+++ bin/docker/install-cmake.sh
@@ -3,7 +3,7 @@
 cmake_version="${1}"
 echo "Installing CMake ${cmake_version}"
 
-curl -o /tmp/cmake.tar.gz -L https://cmake.org/files/v${cmake_version%.*}/cmake-${cmake_version}-Linux-x86_64.tar.gz
+curl -o /tmp/cmake.tar.gz -L https://cmake.org/files/v${cmake_version%.*}/cmake-${cmake_version}-linux-x86_64.tar.gz
 mkdir -p /opt/local/cmake
 tar -C /opt/local/cmake --strip-components=1 -xf /tmp/cmake.tar.gz
 rm -f /tmp/cmake.tar.gz
