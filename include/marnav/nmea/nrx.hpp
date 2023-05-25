358339d656755e599907350de8c942e6b7d00afe
Epidemy <aipdimi@gmail.com>
2023-05-25 13:46:19 +0330

    Refactoring nrx members to maintain consistency

--- include/marnav/nmea/nrx.hpp
+++ include/marnav/nmea/nrx.hpp
@@ -80,42 +80,42 @@ protected:
 	void append_data_to(std::string &, const version &) const override;
 
 private:
-	uint16_t m_number_sentences;
-	uint16_t m_sentence_number;
-	int m_sequential_id;
-	std::optional<message_code> m_message_code;
-	std::optional<frequency_index> m_frequency_index;
-	std::optional<time> m_time_utc;
-	std::optional<date> m_date;
-	std::optional<uint32_t> m_total_characters;
-	std::optional<uint32_t> m_total_bad_characters;
-	std::optional<status> m_status;
-	std::optional<std::string> m_message;
+	uint16_t number_sentences_;
+	uint16_t sentence_number_;
+	int sequential_id_;
+	std::optional<message_code> message_code_;
+	std::optional<frequency_index> frequency_index_;
+	std::optional<time> time_utc_;
+	std::optional<date> date_;
+	std::optional<uint32_t> total_characters_;
+	std::optional<uint32_t> total_bad_characters_;
+	std::optional<status> status_;
+	std::optional<std::string> message_;
 
 public:
-	uint16_t get_number_sentences() const { return m_number_sentences; };
-	uint16_t get_sentence_number() const { return m_sentence_number; };
-	uint8_t get_sequential_id() const { return m_sequential_id; };
-	std::optional<message_code> get_message_code() const { return m_message_code; };
-	std::optional<frequency_index> get_frequency_index() const { return m_frequency_index; };
-	std::optional<time> get_time_utc() const { return m_time_utc; };
-	std::optional<date> get_utc_date() const { return m_date; };
-	std::optional<uint32_t> get_total_characters() const { return m_total_characters; };
-	std::optional<uint32_t> get_total_bad_characters() const { return m_total_bad_characters; };
-	std::optional<status> get_status() const { return m_status; };
-	std::optional<std::string> get_message() const { return m_message; };
+	uint16_t get_number_sentences() const { return number_sentences_; };
+	uint16_t get_sentence_number() const { return sentence_number_; };
+	uint8_t get_sequential_id() const { return sequential_id_; };
+	std::optional<message_code> get_message_code() const { return message_code_; };
+	std::optional<frequency_index> get_frequency_index() const { return frequency_index_; };
+	std::optional<time> get_time_utc() const { return time_utc_; };
+	std::optional<date> get_utc_date() const { return date_; };
+	std::optional<uint32_t> get_total_characters() const { return total_characters_; };
+	std::optional<uint32_t> get_total_bad_characters() const { return total_bad_characters_; };
+	std::optional<status> get_status() const { return status_; };
+	std::optional<std::string> get_message() const { return message_; };
 
-	void set_number_sentences(uint16_t value) { m_number_sentences = value; }
-	void set_sentence_number(uint16_t value) { m_sentence_number = value; }
-	void set_sequential_id(uint8_t value) { m_sequential_id = value; }
-	void set_message_code(const message_code & t) noexcept { m_message_code = t; }
-	void set_frequency_index(frequency_index t) noexcept { m_frequency_index = t; }
-	void set_time_utc(const time & t) noexcept { m_time_utc = t; }
-	void set_date(const date & t) noexcept { m_date = t; }
-	void set_total_characters(uint32_t t) noexcept { m_total_characters = t; }
-	void set_total_bad_characters(uint32_t t) noexcept { m_total_bad_characters = t; }
-	void set_status(status s) noexcept { m_status = s; }
-	void set_message(const std::string & m) noexcept { m_message = m; }
+	void set_number_sentences(uint16_t value) { number_sentences_ = value; }
+	void set_sentence_number(uint16_t value) { sentence_number_ = value; }
+	void set_sequential_id(uint8_t value) { sequential_id_ = value; }
+	void set_message_code(const message_code & t) noexcept { message_code_ = t; }
+	void set_frequency_index(frequency_index t) noexcept { frequency_index_ = t; }
+	void set_time_utc(const time & t) noexcept { time_utc_ = t; }
+	void set_date(const date & t) noexcept { date_ = t; }
+	void set_total_characters(uint32_t t) noexcept { total_characters_ = t; }
+	void set_total_bad_characters(uint32_t t) noexcept { total_bad_characters_ = t; }
+	void set_status(status s) noexcept { status_ = s; }
+	void set_message(const std::string & m) noexcept { message_ = m; }
 
 private:
 	inline std::optional<message_code> fill_message_code(const std::string & m) noexcept;
--- src/marnav/nmea/nrx.cpp
+++ src/marnav/nmea/nrx.cpp
@@ -9,9 +9,9 @@ constexpr const char * nrx::TAG;
 
 nrx::nrx()
 	: sentence(ID, TAG, talker::communications_telex)
-	, m_number_sentences(0u)
-	, m_sentence_number(0u)
-	, m_sequential_id(0)
+	, number_sentences_(0u)
+	, sentence_number_(0u)
+	, sequential_id_(0)
 {
 }
 
@@ -23,16 +23,16 @@ nrx::nrx(talker talk, fields::const_iterator first, fields::const_iterator last)
 	if (std::distance(first, last) != NRX_MESSAGE_FIELDS)
 		throw std::invalid_argument{"invalid number of fields in nrx"};
 
-	read(*(first + 0), m_number_sentences);
-	read(*(first + 1), m_sentence_number);
-	read(*(first + 2), m_sequential_id);
+	read(*(first + 0), number_sentences_);
+	read(*(first + 1), sentence_number_);
+	read(*(first + 2), sequential_id_);
 
 	std::string message_code;
 	read(*(first + 3), message_code);
-	m_message_code = fill_message_code(message_code);
+	message_code_ = fill_message_code(message_code);
 
-	read(*(first + 4), m_frequency_index);
-	read(*(first + 5), m_time_utc);
+	read(*(first + 4), frequency_index_);
+	read(*(first + 5), time_utc_);
 
 	std::optional<uint32_t> d;
 	std::optional<uint32_t> m;
@@ -43,12 +43,12 @@ nrx::nrx(talker talk, fields::const_iterator first, fields::const_iterator last)
 	read(*(first + 8), y);
 
 	if (d && m && y)
-		m_date = nmea::date{*y, to_month(*m), *d};
+		date_ = nmea::date{*y, to_month(*m), *d};
 
-	read(*(first + 9), m_total_characters);
-	read(*(first + 10), m_total_bad_characters);
-	read(*(first + 11), m_status);
-	read(*(first + 12), m_message);
+	read(*(first + 9), total_characters_);
+	read(*(first + 10), total_bad_characters_);
+	read(*(first + 11), status_);
+	read(*(first + 12), message_);
 	check();
 }
 
@@ -70,25 +70,25 @@ void nrx::append_data_to(std::string & s, const version &) const
 	std::optional<uint32_t> m;
 	std::optional<uint32_t> y;
 
-	if (m_date) {
-		d = m_date->day();
-		m = to_numeric(m_date->mon());
-		y = m_date->year();
+	if (date_) {
+		d = date_->day();
+		m = to_numeric(date_->mon());
+		y = date_->year();
 	}
 
-	append(s, format(m_number_sentences, 3));
-	append(s, format(m_sentence_number, 3));
-	append(s, format(m_sequential_id, 2));
-	append(s, to_string(m_message_code));
-	append(s, to_string(m_frequency_index));
-	append(s, to_string(m_time_utc));
+	append(s, format(number_sentences_, 3));
+	append(s, format(sentence_number_, 3));
+	append(s, format(sequential_id_, 2));
+	append(s, to_string(message_code_));
+	append(s, to_string(frequency_index_));
+	append(s, to_string(time_utc_));
 	append(s, format(d, 2));
 	append(s, format(m, 2));
 	append(s, format(y, 4));
-	append(s, to_string(m_total_characters));
-	append(s, to_string(m_total_bad_characters));
-	append(s, to_string(m_status));
-	append(s, to_string(m_message));
+	append(s, to_string(total_characters_));
+	append(s, to_string(total_bad_characters_));
+	append(s, to_string(status_));
+	append(s, to_string(message_));
 }
 
 inline std::optional<nrx::message_code> nrx::fill_message_code(const std::string & m) noexcept
@@ -107,13 +107,13 @@ inline std::optional<nrx::message_code> nrx::fill_message_code(const std::string
 
 void nrx::check() const
 {
-	if ((m_number_sentences < 1) || (m_number_sentences > 999))
+	if ((number_sentences_ < 1) || (number_sentences_ > 999))
 		throw std::invalid_argument{"invalid number of sentences"};
 
-	if ((m_sentence_number < 1) || (m_sentence_number > 999))
+	if ((sentence_number_ < 1) || (sentence_number_ > 999))
 		throw std::invalid_argument{"invalid sentence number"};
 
-	if ((m_sequential_id < 0) || (m_sequential_id > 99))
+	if ((sequential_id_ < 0) || (sequential_id_ > 99))
 		throw std::invalid_argument{"invalid sequential message number"};
 }
 }
